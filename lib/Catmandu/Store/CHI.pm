e1a856784d92316869144db1bfd01f4322748cac
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2014-06-28 08:32:10 +0200

    Adding milla, and tests

--- lib/Catmandu/Store/CHI.pm
+++ lib/Catmandu/Store/CHI.pm
@@ -1,68 +1,69 @@
-package Catmandu::Store::CHI::Bag;
+=head1 NAME
 
-use Moo;
-use Data::UUID;
-use Data::Dumper;
+Catmandu::Store::CHI - a CHI backed caching store
 
-with 'Catmandu::Bag';
+=head1 SYNOPSIS
 
-sub generator {
-    my $self = shift;
-    my $keys = [ $self->store->chi->get_keys ];
-    sub {
-        my $id = pop @$keys;
+   # From the command line
+   $ catmandu export CHI --driver File --root_dir /data to YAML
+   $ catmandu import JSON to CHI --driver File --root_dir /data < data.json
 
-        return undef unless $id;
+   # From perl
+   use Catmandu;
 
-        $self->get($id);
-    };
-}
+   my $store = Catmandu->store('CHI', driver => 'File' , root_dir => '/data');
 
-sub get {
-    my ($self,$id) = @_;
-    $self->store->chi->get($id);
-}
+   $store->bag->each(sub {
+        my $item = shift;
+        ...
+   });
 
-sub add {
-    my ($self,$data) = @_;
-    $data->{_id} //= gen_id();
-    my $id = $data->{_id};
-    $self->store->chi->set($id,$data);
-    return $data;
-}
+   $store->bag->add({ test => 123 });
 
-sub delete {
-    my ($self,$id) = @_;
-    $self->store->chi->remove($id);
-}
+=head1 METHODS
 
-sub delete_all {
-    my ($self) = @_;
-    $self->store->chi->clear();
-}
+=head2 new()
 
-sub gen_id {
-    Data::UUID->new->create_str();
-}
+=head2 new(driver => $chi_driver, [OPT => VAL, OPT2 => VAL])
 
+Create a new Catmandu::Store::CHI with a $chi_driver and optional parameters. When no driver is given
+then by default the 'Memory' driver will be used. See L<CHI> for more documentation on possible drivers.
+
+=head2 bag($name)
+ 
+Create or retieve a bag with name $name. Returns a L<Catmandu::Bag>.
+
+=head1 SEE ALSO
+
+L<CHI>, L<Catmandu> , L<Catmandu::Store> , L<Catmandu::Bag>
+
+=head1 AUTHOR
+ 
+Patrick Hochstenbach, C<< <patrick.hochstenbach at ugent.be> >>
+ 
+=head1 LICENSE AND COPYRIGHT
+ 
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+ 
+See http://dev.perl.org/licenses/ for more information.
+ 
+=cut
 package Catmandu::Store::CHI;
+
+{
+  $Catmandu::Store::CHI::VERSION = '0.01';
+}
 
 use Moo;
 use CHI;
+use Catmandu::Store::CHI::Bag;
 
 with 'Catmandu::Store';
 
 has 'driver' => (is => 'ro' , required => 1 , default => sub { 'Memory' });
 has 'opts'   => (is => 'rw');
-has 'chi'    => (is => 'ro' , lazy => 1 , builder => 1);
-
-sub _build_chi {
-    my ($self) = @_;
-    my $driver = $self->driver;
-    my $opts   = $self->opts;
-
-    CHI->new(driver => $driver, %$opts);
-}
 
 sub BUILD {
     my ($self,$opts) = @_;
